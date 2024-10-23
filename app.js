ce40e8c7d28267fdffcb89b7752477f81cfc52fc
ejlong90 <ejlong21@gmail.com>
2024-10-22 21:06:00 -0400

    first commit for this project

---
+++ app.js
@@ -0,0 +1,8 @@
+let board = ["", "", "", "", "", "", "", "", ""]; // 9 empty cells
+let currentPlayer = "X"; // X starts first
+
+function displayBoard() {
+    console.log(board[0], board[1], board [2]);
+    console.log(board[3], board[4], board [5]);
+    console.log(board[6], board[7], board [8]);
+}
